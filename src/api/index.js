37dc3556048b96535a346d73ea49c42bdffae593
Simon Sinding <sinding2000@gmail.com>
2017-04-25 20:06:29 +0200

    added rooms endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ import facets from './facets';
 import calendars from './calendars';
 import events from './events';
 import calendarsAdmin from './calendarsAdmin';
+import rooms from './rooms';
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -12,6 +13,7 @@ export default ({ config, db }) => {
 	api.use('/calendars', calendars({ config, db }));
 	api.use('/calendars/:calid/events', events({ config, db }));
 	api.use('/admin/calendars', calendarsAdmin({ config, db }));
+	api.use('/rooms', rooms({ config, db }));
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
