cc568abc675420dfaa1b07c79c4dd645edc9304b
Simon Sinding <sinding2000@gmail.com>
2017-04-28 12:31:33 +0200

    cleaned up model

--- src/models/rooms.js
+++ src/models/rooms.js
@@ -1,42 +1,17 @@
-import authorizer from '../auth';
-import squel from 'squel';
-
-let sqlBuilder = squel.useFlavour('postgres');
-
-let client = null;
-//TODO: Err handling, this is only here temporaly
-let err = null;
-
+import database from '../database';
 
 const roomsModel = {
-    openConnection: () => {
-        authorizer.connectDatabase((err, connection) => {
-            if(err) throw(err);
-            client = connection;
-        });
-    },
     create: (roomData, callback) => {
-        roomsModel.openConnection();
-        client.connect();
-        let statement = roomsModel.statementConstructor('INSERT', "rooms", roomData);
-        let query = client.query(statement);
-
-
+        let statement = database.statementConstructor('INSERT', "rooms", roomData);
+        database.query(statement, (err, data) => {
+            callback(err, data.rowCount > 0 ? { "Success": "Room created" } : null);
+        });
     },
     read:   (room, callback) => {
-        roomsModel.openConnection();
-        client.connect();
-        let statement = roomsModel.statementConstructor('SELECT', "rooms", room);
-        let query = client.query(statement);
-        query.on('end', (result) => {
-            if(result.rows){
-                callback(err, result.rows[0]);
-            } else {
-                callback(err, {"error": "no rooms in database"});
-            }
-            client.end();
+        let statement = database.statementConstructor('SELECT', "rooms", room);
+        database.query(statement, (err, data) => {
+            callback(err, result.rows[0]);
         });
-        
     },
     update: (room, callback) => {
             
@@ -45,49 +20,12 @@ const roomsModel = {
             
     },
     list: (callback) =>{
-        roomsModel.openConnection();
-        client.connect();
-        let statement = roomsModel.statementConstructor('SELECT', "rooms");
-        let query = client.query(statement);
-        query.on('end', (result) => {
-            if(result.rows){
-                callback(err, result.rows);
-            } else {
-                callback(err, {"error": "no rooms in database"});
-            }
-            client.end();
-        });
-    },
-    statementConstructor: (type, table, room) => {
-        let sql = "";
-        switch(type){
-            case 'INSERT':
-                sql = sqlBuilder.insert()
-                .into(table)
-                .set('room_name', room.name)
-                .set('room_description', room.description)
-                .set('room_calendar_id', room.calendarId)
-                .toString();
-                return sql;            
-            case 'UPDATE':
+        let statement = database.statementConstructor('SELECT', "rooms");
+        database.query(statement, (err, data) => {
+            callback(err, data.rows.length > 0  ? data.rows : { "Error": "No rooms" });
+         });
+        },
+ 
+};
 
-            break;
-            case 'SELECT':
-                if(room != null){
-                    sql = sqlBuilder.select()
-                    .from(table)
-                    .where('room_id = ' + room.id)
-                    .toString();
-                } else {
-                    sql = sqlBuilder.select()
-                    .from(table)
-                    .toString();
-                }
-                return sql;
-            case 'DROP':
-
-            break;
-        }
-    }
-}
 export default roomsModel;
