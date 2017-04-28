67748330edbb8773872b748c55fa7d3fca0f15c4
Simon Sinding <sinding2000@gmail.com>
2017-04-28 15:53:14 +0200

    cleaned and moved code

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,5 @@
 import { pgQuery, pgConnection } from './clients/postgresClient';
 import config from '../config';
-import squel from 'squel';
-
-let sqlBuilder = squel.useFlavour(config.databaseType);
-
 
 const database = {
     query: (sql,callback) => {
@@ -12,38 +8,6 @@ const database = {
 
     connection: (callback) => {
         pgConnection(callback);
-    },
-
-    statementConstructor: (type, table, room) => {
-        let sql = "";
-        switch(type){
-            case 'INSERT':
-                sql = sqlBuilder.insert()
-                .into(table)
-                .set('room_name', room.name)
-                .set('room_description', "\"" + room.description + "\"")
-                .set('room_calendar_id', room.calendarId)
-                .toString();
-                return sql;            
-            case 'UPDATE':
-
-            break;
-            case 'SELECT':
-                if(room != null){
-                    sql = sqlBuilder.select()
-                    .from(table)
-                    .where('room_id = ' + room.id)
-                    .toString();
-                } else {
-                    sql = sqlBuilder.select()
-                    .from(table)
-                    .toString();
-                }
-                return sql;
-            case 'DROP':
-
-            break;
-        }
     }
 };
 
