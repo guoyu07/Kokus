5e5aa04fee5518f1baf8814a9689eea3c4f7ffa5
Simon Sinding <sinding2000@gmail.com>
2017-04-30 02:18:25 +0200

    added token generator

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,22 +1,41 @@
+import jwt from 'jsonwebtoken';
+import database from '../database';
+import secret from '../credentials/jwtsecret';
+import sqlConstructor from '../lib/statementConstructor';
 import googleJwtClient from './google/googleJwtClient';
 import googleUserClient from './google/googleUserClient';
 
 import config from '../config';
 
 const authorizer = {
-    authorize(callback){
-        switch(config.authorizeMethod.google){
-            case 'jwt': 
-                googleJwtClient.authorize((err, authorizedClient) => {
-                    callback(err, authorizedClient);
-                });
-                break;
-            case 'client':
-                googleUserClient.authorize((err, authorizedClient) => {
-                    callback(err, authorizedClient);
-                });
-                break;            
+    google: {
+        authorize(callback){
+            switch(config.authorizeMethod.google){
+                case 'jwt': 
+                    googleJwtClient.authorize((err, authorizedClient) => {
+                        callback(err, authorizedClient);
+                    });
+                    break;
+                case 'client':
+                    googleUserClient.authorize((err, authorizedClient) => {
+                        callback(err, authorizedClient);
+                    });
+                    break;            
+            }
         }
+    },
+    authorizeApi: (key, callback) => {
+        let statement = sqlConstructor.select('apikeys', { 'key': key });
+        database.query(statement, (err, res) => {
+            if (err) throw(err);
+            if(res.rowCount > 0){
+                jwt.sign(res.rows[0], secret.secret, (err, token) => {
+                    callback(err, token);
+                });
+            } else {
+                callback({'status':'error', 'message': 'API Key does not exist!'}, null)
+            }
+        });
     }
 };
 
