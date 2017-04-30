9f090fb0c20a04803a75bc42d01f9802065de064
Simon Sinding <sinding2000@gmail.com>
2017-04-30 02:30:19 +0200

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@ cd ~/clones/HoneyBase/
 pm2 stop HoneyBase # Stop HoneyBase
 git fetch --all # Fetch the new changes to master
 git reset --hard origin/master # Something
+rm dist -r
 yarn install # Check new dependencies
-npm build # Build production version
+yarn build # Build production version
 pm2 start dist/index.js --name "HoneyBase" # Attempt to start it
 pm2 save # Save the process
 
