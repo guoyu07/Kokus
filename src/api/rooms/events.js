f98eb0dcabd5f9dfcb0125a5df6c177f3fee4b1b
Simon Sinding <sinding2000@gmail.com>
2017-05-01 23:43:49 +0200

    A good clean, last one..

--- src/api/rooms/events.js
+++ src/api/rooms/events.js
@@ -1,6 +1,5 @@
 import resource from 'resource-router-middleware';
 import roomEventsModel from '../../models/rooms/roomEvents';
-import jsend from 'jsend';
 
 export default ({ config, db }) => resource({
 
@@ -14,7 +13,7 @@ export default ({ config, db }) => resource({
 		// Get the room id from urls TODO: research a better way!
 		let roomId = req.baseUrl.split("/")[3];
 		roomEventsModel.read({ 'id': eventId, 'room_id': roomId }, (err, result) => {
-			err = result.rowCount !== 0 ? null : jsend.error('Event with ID ' + eventId + ' doesnt exist!');
+			err = result.rowCount !== 0 ? null : {'status': 'error', 'message': 'Event with ID ' + eventId + ' doesnt exist!'};
 			callback(err, result.rows[0]);
 		});
 	},
@@ -23,7 +22,8 @@ export default ({ config, db }) => resource({
 	index({ baseUrl }, res) {
 		let roomId = baseUrl.split("/")[3];
 		roomEventsModel.list({ 'room_id': roomId }, (err, data) => {
-			err ? res.send(jsend.error(err)) : res.send(jsend.success(data.rows));
+			if(err) return res.jsend.error(err);
+			res.jsend.success(data);
 		});
 	},
 
@@ -31,27 +31,29 @@ export default ({ config, db }) => resource({
 	create({ body, baseUrl }, res) {
 		body.room_id = baseUrl.split("/")[3]; 		
 		roomEventsModel.create(body, (err, data) => {
-			data = err ? jsend.error(err) : jsend.success(data);
-			res.json(data);
+			if(err) return res.jsend.error(err);
+			 res.jsend.success(data);
 		});
 	},
 
 	/** GET /:id - Return a given entity */
 	read({ eventId }, res) {
-		res.json(eventId);
+		res.jsend.success(eventId);
 	},
 
 	/** PUT /:id - Update a given entity */
 	update({ body, params }, res) {
 		roomEventsModel.update({ 'id': params.eventId } , body, (err, data) => {
-			err ? res.send(jsend.error(data)) : res.status(204).send(jsend.success(data));
+			if(err) return res.jsend.error(err);
+			res.jsend.success(data);
 		});
 	},
 
 	/** DELETE /:id - Delete a given entity */
 	delete({ params }, res) {
 		roomEventsModel.delete({'id': params.eventId }, (err, data) => {
-			err ? res.send(jsend.error(data)) : res.status(204).send(jsend.success(data));
+			if(err) return res.jsend.error(err);
+			res.jsend.success(data);
 		});
 	}
 });
--- src/api/rooms/rooms.js
+++ src/api/rooms/rooms.js
@@ -1,6 +1,5 @@
 import resource from 'resource-router-middleware';
 import roomsModel from '../../models/rooms/rooms';
-import jsend from 'jsend';
 
 export default ({ config }) => resource({
 
@@ -13,7 +12,7 @@ export default ({ config }) => resource({
 	load(req, id, callback) {
 		roomsModel.read({ room_id: id }, (err, result) => {
 			// Check if the result from the database has any rows.
-			err = result.rowCount !== 0 ? null : jsend.error('Room with ID ' + id + ' doesnt exist!');
+			err = result.rowCount !== 0 ? null :  {'status': 'error', 'message': 'Room with ID ' + id + ' doesnt exist!'};
 			callback(err, result.rows[0]);
 		});
 	},
@@ -21,7 +20,8 @@ export default ({ config }) => resource({
 	/** GET / - List all entities */
 	index({ params }, res) {
 		roomsModel.list((err, data) => {
-			err ? res.send(jsend.error(err)) : res.send(jsend.success(data.rows));
+			if(err) return res.jsend.error(err);
+			res.jsend.success(data);
 		});
 	},
 
@@ -29,13 +29,14 @@ export default ({ config }) => resource({
 	create({ body }, res) {
 		// The room details is in body
 		roomsModel.create(body, (err, data) => {
-			err ? res.send(jsend.error(err)) : res.send(jsend.success(data));
+			if(err) return res.jsend.error(err);
+			res.jsend.success(data);
 		});
 	},
 
 	/** GET /:id - Return a given entity */
 	read({ roomId }, res) {
-		res.send(jsend.success(roomId));
+		res.jsend.success(data);
 	},
 
 	/** PUT /:id - Update a given entity */
