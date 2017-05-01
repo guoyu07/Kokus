aa723ba21589f8a8e3e4106cfa18965b92a148b2
Simon Sinding <sinding2000@gmail.com>
2017-05-01 16:18:55 +0200

    Small fixes

--- src/api/authenticate.js
+++ src/api/authenticate.js
@@ -4,37 +4,31 @@ import authorizer from '../auth';
 export default ({ config, db }) => resource({
 	/** GET / - List all entities */
 	index({ body }, res) {
-        res.json({
-            'status': 'error',
-            'message': 'This route does not support GET'
-        });
+        res.jsend.error({'message': 'This route does not support GET'});
 	},
 	/** POST / - Create a new entity */
 	create({ body }, res) {
-        authorizer.authorizeApi(body.key, (err, token) => {
-            if(token){
-                res.json({
-                   success: true,
-                   message: 'Token will expire in ' + config.jwt.expire + ' minutes',
-                   token
-                });
-            } else {
-                res.json(err);
-            }
-        });
+        if(body.key){
+            authorizer.authorizeApi(body.key, (err, token) => {
+                if(token){
+                    res.jsend.success({
+                    expires:  config.jwt.expire,
+                    token: token
+                    });
+                } else {
+                    res.jsend.error(err);
+                }
+            });
+        } else {
+           res.jsend.error("No API key in body");
+        }
 	},
 	/** PUT /:id - Update a given entity */
 	update({ body }, res) {
-        res.json({
-            'status': 'error',
-            'message': 'This route does not support GET'
-        });
+        res.jsend.error({'message': 'This route does not support PUT'});
 	},
 	/** DELETE /:id - Delete a given entity */
 	delete({ body }, res) {
-        res.json({
-            'status': 'error',
-            'message': 'This route does not support GET'
-        });
+        res.jsend.error({'message': 'This route does not support DELETE'});
 	}
 });
