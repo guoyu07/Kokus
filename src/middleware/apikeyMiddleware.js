cf2b90994812e32c9aebff92bffee22332ace7f2
Simon Sinding <sinding2000@gmail.com>
2017-05-01 23:44:56 +0200

    Smaller, prettier

--- src/middleware/apikeyMiddleware.js
+++ src/middleware/apikeyMiddleware.js
@@ -3,22 +3,13 @@ import secret from '../credentials/jwtsecret';
 
 const apikeyMiddleware = (req, res, next) => {
     let token = req.body.token || req.query.token || req.headers['x-access-token'];
-    if (token) {
-        // Verify the token from headers
-        jwt.verify(token, secret.secret, function(err, decoded) {      
-            if (err) {
-                return res.json({ success: false, message: 'Failed to authenticate token.' });    
-            } else {
-                req.decoded = decoded;    
-                next();
-            }
-        });
-    } else {
-        return res.status(403).send({ 
-            success: false, 
-            message: 'No token provided.' 
-        });
-    }
+    if (!token) return res.status(403).jsend.error('No token provided.');
+    // Verify the token from headers
+    jwt.verify(token, secret.secret, function(err, decoded) {      
+        if (err) return res.jsend.error('Failed to authenticate token.');
+        req.decoded = decoded;
+        next();
+    });
 };
 
 export default apikeyMiddleware;
