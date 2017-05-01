121ab264d69765dff88756a78571e19e5180dd9c
Simon Sinding <sinding2000@gmail.com>
2017-05-01 16:23:37 +0200

    Fixed errors

--- src/api/settings/keys.js
+++ src/api/settings/keys.js
@@ -3,46 +3,38 @@ import resource from 'resource-router-middleware';
 export default ({ config, db }) => resource({
 
 	/** Property name to store preloaded entity on `request`. */
-	id : 'facet',
+	id : 'apikey',
 
 	/** For requests with an `id`, you can auto-load the entity.
 	 *  Errors terminate the request, success sets `req[id] = data`.
 	 */
 	load(req, id, callback) {
-	
-		callback(err, facet);
+		let err = null;
+		callback(err, id);
 	},
 
 	/** GET / - List all entities */
 	index({ params }, res) {
-		res.json(facets);
+		res.json({"blank": "blank"});
 	},
 
 	/** POST / - Create a new entity */
 	create({ body }, res) {
-		body.id = facets.length.toString(36);
-		facets.push(body);
-		res.json(body);
+		res.json({"blank": "blank"});
 	},
 
 	/** GET /:id - Return a given entity */
 	read({ facet }, res) {
-		res.json(facet);
+		res.json({"blank": "blank"});
 	},
 
 	/** PUT /:id - Update a given entity */
-	update({ facet, body }, res) {
-		for (let key in body) {
-			if (key!=='id') {
-				facet[key] = body[key];
-			}
-		}
+	update({ body }, res) {
 		res.sendStatus(204);
 	},
 
 	/** DELETE /:id - Delete a given entity */
 	delete({ facet }, res) {
-		facets.splice(facets.indexOf(facet), 1);
 		res.sendStatus(204);
 	}
 });
