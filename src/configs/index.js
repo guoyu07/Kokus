2f78e3c23d19b611d5c06d2b02590d8105ad512a
Simon Sinding <sinding2000@gmail.com>
2017-05-02 15:18:31 +0200

    added paths.. Needs a redo

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,11 @@
 import serverConfig from './server';
 import databaseConfig from './database';
 
+export const paths = {
+    serverConfigPath: __dirname + '/server.json',    
+    databaseConfigPath: __dirname + '/database.json'
+};
+
 export {
     serverConfig,
     databaseConfig
