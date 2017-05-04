f6ab932fc2e23778299b774cb873ee7c2dd2f756
Simon Sinding <sinding2000@gmail.com>
2017-05-04 15:50:27 +0200

    middleware

--- src/api/index.js
+++ src/api/index.js
@@ -8,19 +8,18 @@ import rooms from './rooms/rooms';
 import roomEvents from './rooms/events';
 import settings from './settings';
 import apikeyMiddleware from '../middleware/apikeyMiddleware';
+import scopeMiddleware from '../middleware/scopeMiddleware';
 
 export default ({ config, db }) => {
 	let api = Router();
-	api.get('/*',function(req,res,next){
-		res.setHeader('Content-Type','application/json');
-		next();
-	});
 	// Make sure authenticate is not a protected route
 	// eg it has to be above the middleware
 	api.use('/authenticate', authenticate({ config }));
 	
 	// check api key
 	api.use(apikeyMiddleware);
+	// api.use(scopeMiddleware);
+	
 	
 	// mount the facets resource
 	api.use('/calendars', calendars({ config }));
